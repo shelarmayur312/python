52a0f07d6778dcac6957f24a3c5d70b75192595a
shelarmayur312 <156174171+shelarmayur312@users.noreply.github.com>
2024-06-25 07:59:59 -0700

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,20 @@
+text = False
+
+# print("student")
+# print(text)
+
+text = False
+text = 5
+Text = 6
+
+frist_name = "mayur"
+
+
+name = "mayur"
+Name = "vishal"
+
+print(name)
+
+pass_exam = 6
+
+age = 6
