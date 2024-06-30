09d63c65441bcff0051bc715ea50d886c8560d01
shelarmayur312 <156174171+shelarmayur312@users.noreply.github.com>
2024-06-30 01:28:04 -0700

    Add files via upload

---
+++ number.py
@@ -0,0 +1,14 @@
+num1 = 7 #integer
+
+num2 = 2.5 #float
+
+result = num1 + num2 #add
+result = num1 - num2 #subtract
+result = num1 * num2 #multiply
+result = num1 / num2 #division
+result = num1 // num2 #when two  int are divided result in int 
+result = num1 ** num2 #power
+
+result = num1 % num2 # remainder
+  
+print(result)
